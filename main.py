7d08ed06de949004f4b3c45a89b2514771fd80ef
raniamehria <rania.mehria.pro@gmail.com>
2025-11-13 09:38:52 +0100

    model coice

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
+import joblib
+import numpy as np
+from flask import Flask, request, jsonify
 
-# Chargement simple (on mettra le bon chemin après avoir mis le CSV)
-df = pd.read_csv("data/heart_failure.csv")
-print("Shape:", df.shape)
-print(df.head(3))
+bundle = joblib.load("model.pkl")
+model = bundle["model"]
+scaler = bundle["scaler"]
+feature_names = bundle["features"]
 
-# Exemple minimal : adaptera plus tard selon les colonnes réelles
-y = df["target"]
-X = df.drop(columns=["target"])
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+app = Flask(__name__)
 
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-print(classification_report(y_test, y_pred))
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    features = np.array(data["features"]).reshape(1, -1)
+
+    # normalisation
+    features_scaled = scaler.transform(features)
+
+    prediction = model.predict(features_scaled)
+    return jsonify({"prediction": int(prediction[0])})
+
+if __name__ == "__main__":
+    app.run(debug=True)
